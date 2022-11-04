a37e43e91d8b3c3d914358da4c8a68b929099386
refresh88 <ouok88@gmail.com>
2022-11-04 17:12:33 +0900

    JS: nav toggle fix

--- main.js
+++ main.js
@@ -12,6 +12,24 @@ document.addEventListener('scroll', () => {
   }
 });
 
+// // Handle scrolling when tapping on the navbar menu
+// const navbarMenu = document.querySelector('.navbar__menu');
+// navbarMenu.addEventListener('click', (event) => {
+//   const target = event.target;
+//   const link = target.dataset.link;
+//   if (link == null) {
+//     return;
+//   }
+//   navbarMenu.classList.remove('open');
+//   scrollIntoView(link);
+// });
+
+// // Navbar toggle button for small screen
+// const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+// navbarToggleBtn.addEventListener('click', () => {
+//   navbarMenu.classList.toggle('open');
+// });
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
